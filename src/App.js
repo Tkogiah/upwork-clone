c8924ea14807faec03944d2a6c9403f031896642
Marcus Overstreet <tkogiah@gmail.com>
2022-07-17 19:42:00 -0700

    initial commit of upwork clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Login from "./Login.js";
+import JobBoard from "./JobBoard";
+import JobDetail from "./JobDetail";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/jobs" element={<JobBoard />} />
+        <Route path="/job/:id" element={<JobDetail />} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
